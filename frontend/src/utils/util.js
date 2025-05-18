a94887ae7fb38a3bc2520449bcdab3eb16a67889
quranaaulia <162184412+quranaaulia@users.noreply.github.com>
2025-05-18 22:07:57 +0700

    Update util.js

--- frontend/src/utils/util.js
+++ frontend/src/utils/util.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:5000";
-export const BASE_URL_API = "http://localhost:5000/api"
+export const BASE_URL = "https://notes-be183-651881126563.us-central1.run.app";
+export const BASE_URL_API = "https://notes-be183-651881126563.us-central1.run.app/api"
